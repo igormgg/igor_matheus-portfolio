4e306abfb9af2cc0edfde730961c2095b2eb0aa1
Igor <igormgg@hotmail.com>
2021-12-23 20:49:49 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./reset.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">My Portfolio</header>
+    </div>
+  );
+}
+
+export default App;
