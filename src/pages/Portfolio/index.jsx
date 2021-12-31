83f1b71faf34cb006155837035fb5d98d14a8a7b
Igor <igormgg@hotmail.com>
2021-12-31 00:21:30 -0300

    feat: portfolio page done

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,12 +1,136 @@
 import Header from "../../components/Header";
 import PortfolioContainer from "./styles";
+import devSvg from "../../img/devSvg.svg";
+import pcBuilder from "../../img/pcBuilder.png";
+import hamburgueriaKenzie from "../../img/hamburgueriaKenzie.png";
+import devHabits from "../../img/devHabits.png";
+import kenzieHub from "../../img/kenzieHub.png";
+import ligBallZ from "../../img/ligBallZ.png";
+import torreDeHanoi from "../../img/torreDeHanoi.png";
+import pokemonJackpot from "../../img/pokemonJackpot.png";
+import { BsGithub } from "react-icons/bs";
 
 const Portfolio = () => {
   return (
     <>
       <Header />
       <PortfolioContainer>
-        <h1>Portfólio</h1>
+        <div id="upperDiv">
+          <div id="introDiv">
+            <h1>Veja alguns dos meus últimos projetos!</h1>
+            <p>
+              O código de todos os projetos se encontram no meu repositório do
+              github. Sinta-se livre para visitar todos eles: {""}
+              <a
+                href="https://github.com/igormgg?tab=repositories"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsGithub />
+              </a>
+            </p>
+          </div>
+          <div id="devSvgDiv">
+            <img src={devSvg} alt="Dev" />
+          </div>
+        </div>
+        <div id="projectsDiv">
+          <div className="projectBox">
+            <img
+              src={pcBuilder}
+              alt="Pc Builder"
+              onClick={() =>
+                window.open(
+                  "https://pc-builder-capstone-q2.vercel.app/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>PC Builder</h2>
+            <p>Plataforma de venda de hardware</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={hamburgueriaKenzie}
+              alt="Hamburgueria Kenzie"
+              onClick={() =>
+                window.open(
+                  "https://react-entrega-s5-hamburgueria-2-0-com-typescript-json-s-igormgg.vercel.app/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>Hamburgueria Kenzie</h2>
+            <p>Plataforma de venda de hambúrguer</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={devHabits}
+              alt="Dev Habits"
+              onClick={() =>
+                window.open("https://habits-project-g2.vercel.app/", "_blank")
+              }
+            />
+            <h2>Dev Habits</h2>
+            <p>Gerenciamento de hábitos</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={kenzieHub}
+              alt="Kenzie Hub"
+              onClick={() =>
+                window.open(
+                  "https://react-entrega-s2-kenzie-hub-igormgg.vercel.app/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>Kenzie Hub</h2>
+            <p>Cadastro de tecnologias</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={ligBallZ}
+              alt="Lig Ball Z"
+              onClick={() =>
+                window.open(
+                  "https://kenzie-academy-brasil-developers.github.io/entrega-lig-4-sprint-5-igormgg/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>Lig Ball Z</h2>
+            <p>Jogo temático Lig-4</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={torreDeHanoi}
+              alt="Torre de Hanói"
+              onClick={() =>
+                window.open(
+                  "https://kenzie-academy-brasil-developers.github.io/entrega-torre-de-hanoi-sprint-5-GuilherminoL/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>Torre de Hanói</h2>
+            <p>Jogo temático Torre de Hanói</p>
+          </div>
+          <div className="projectBox">
+            <img
+              src={pokemonJackpot}
+              alt="Pokémon Jackpot"
+              onClick={() =>
+                window.open(
+                  "https://kenzie-academy-brasil-developers.github.io/entrega-construa-um-jogo-de-azar-sprint-3-igormgg/",
+                  "_blank"
+                )
+              }
+            />
+            <h2>Pokémon Jackpot</h2>
+            <p>Jogo temático Jackpot</p>
+          </div>
+        </div>
       </PortfolioContainer>
     </>
   );
--- src/pages/Portfolio/styles.jsx
+++ src/pages/Portfolio/styles.jsx
@@ -4,8 +4,145 @@ const PortfolioContainer = styled.div`
   max-width: 1200px;
   width: 100%;
   height: 100%;
-  overflow-x: hidden;
-  background-color: purple;
+  padding: 12px;
+  overflow: hidden;
+
+  #upperDiv {
+    display: flex;
+    margin-top: 32px;
+
+    #introDiv {
+      width: 50%;
+      color: var(--gray4);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+      animation: left_to_right 1s 1 ease 0.3s both;
+
+      h1 {
+        max-width: 400px;
+        width: 75%;
+        font-size: 40px;
+        font-weight: bold;
+      }
+
+      p {
+        max-width: 400px;
+        width: 75%;
+        font-size: 18px;
+
+        a {
+          svg {
+            color: var(--gray4);
+          }
+        }
+      }
+    }
+
+    #devSvgDiv {
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      animation: right_to_left 1s 1 ease 0.6s both;
+
+      img {
+        min-width: 350px;
+        width: 65%;
+      }
+    }
+  }
+
+  #projectsDiv {
+    margin-top: 24px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    gap: 56px;
+    animation: go_up 1s 1 ease 0.9s both;
+
+    .projectBox {
+      max-width: 350px;
+      min-width: 250px;
+      width: 100%;
+      padding: 12px;
+      background-color: var(--white);
+      color: var(--gray4);
+      box-shadow: 0px 5px 8px var(--gray4);
+      border-radius: 8px;
+      :hover {
+        cursor: pointer;
+        background-color: var(--green);
+        color: var(--white);
+      }
+
+      img {
+        width: 100%;
+      }
+
+      h2 {
+        font-size: 20px;
+        font-weight: bold;
+      }
+
+      p {
+        font-size: 16px;
+      }
+    }
+  }
+
+  @media screen and (max-width: 750px) {
+    #upperDiv {
+      margin-top: 16px;
+      flex-direction: column;
+      align-items: center;
+      gap: 24px;
+
+      #introDiv {
+        width: unset;
+        gap: 16px;
+
+        h1 {
+          width: unset;
+        }
+
+        p {
+          width: unset;
+        }
+      }
+
+      #devSvgDiv {
+        min-width: 300px;
+        width: 50%;
+
+        img {
+          min-width: unset;
+          width: 100%;
+        }
+      }
+    }
+  }
+
+  @keyframes left_to_right {
+    from {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
+  }
+
+  @keyframes right_to_left {
+    from {
+      opacity: 0;
+      transform: translateX(100px);
+    }
+  }
+
+  @keyframes go_up {
+    from {
+      opacity: 0;
+      transform: translateY(100px);
+    }
+  }
 `;
 
 export default PortfolioContainer;
